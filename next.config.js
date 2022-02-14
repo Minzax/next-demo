f807ce3287414c4c7f7b4c5cd087734ba816cba4
minzax <minzax83@outlook.com>
2022-02-14 12:59:22 +0800

    11

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://yapi.valsn.com/mock/11/api/:path*',
+        // destination: 'https://dev-api.olicharity.org/api/:path*',
+        // destination: 'https://e5f4-183-14-212-239.ngrok.io/api/:path*',
+        // destination: 'http://127.0.0.1/api/:path*',
+      },
+    ]
+  },
 }
